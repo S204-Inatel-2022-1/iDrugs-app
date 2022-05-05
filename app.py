bc1ba2b2cef12e1a6ae1dfbc9d5e3a59b288cdcb
Ítalo Moura <italo.moura1201@gmail.com>
2022-05-05 00:13:20 -0300

    change port

--- app.py
+++ app.py
@@ -71,5 +71,5 @@ def nao_entre_em_panico():
 app.register_blueprint(blueprint)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
