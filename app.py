2df3103ae30013ae6b958b5d72554e49062438ab
Ítalo Moura <italo.moura1201@gmail.com>
2022-05-04 22:16:20 -0300

    Configuração de rotas e ambientes para rodar o app

--- app.py
+++ app.py
@@ -10,55 +10,55 @@ import requests
 blueprint = Blueprint('app', __name__, url_prefix='/idrugs-app')
 
 # USER
-@blueprint.route('/product', methods=['POST', 'PUT', 'DELETE', 'GET'])
+@blueprint.route('/pharma/product', methods=['POST', 'PUT', 'DELETE', 'GET'])
 def CRUD_product_route():
     url = pharma_engine + 'product'
     if request.method == 'POST':
         data = json.dumps(request.json)
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data)
     elif request.method == 'PUT':
         data = json.dumps(request.json)
-        response = requests.put(url, data=data)
+        response = requests.put(url, json=data)
     elif request.method == 'DELETE':
         data = json.dumps(request.json)
-        response = requests.delete(url, data=data)
+        response = requests.delete(url, json=data)
     elif request.method == 'GET':
         data = json.dumps(request.args)
-        response = requests.get(url, data=data)
+        response = requests.get(url, json=data)
     return Response(response, mimetype='application/json', status=response.status_code)
 
-@blueprint.route('/user', methods=['POST', 'PUT', 'DELETE', 'GET'])
+@blueprint.route('/pharma/user', methods=['POST', 'PUT', 'DELETE', 'GET'])
 def CRUD_user_route():
     url = pharma_engine + 'user'
     if request.method == 'POST':
         data = json.dumps(request.json)
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data)
     elif request.method == 'PUT':
         data = json.dumps(request.json)
-        response = requests.put(url, data=data)
+        response = requests.put(url, json=data)
     elif request.method == 'DELETE':
         data = json.dumps(request.json)
-        response = requests.delete(url, data=data)
+        response = requests.delete(url, json=data)
     elif request.method == 'GET':
         data = json.dumps(request.args)
-        response = requests.get(url, data=data)
+        response = requests.get(url, json=data)
     return Response(response, mimetype='application/json', status=response.status_code)
 
-@blueprint.route('/type', methods=['POST', 'PUT', 'DELETE', 'GET'])
+@blueprint.route('/pharma/type', methods=['POST', 'PUT', 'DELETE', 'GET'])
 def CRUD_type_route():
     url = pharma_engine + 'type'
     if request.method == 'POST':
         data = json.dumps(request.json)
-        response = requests.post(url, data=data)
+        response = requests.post(url, json=data)
     elif request.method == 'PUT':
         data = json.dumps(request.json)
-        response = requests.put(url, data=data)
+        response = requests.put(url, json=data)
     elif request.method == 'DELETE':
         data = json.dumps(request.json)
-        response = requests.delete(url, data=data)
+        response = requests.delete(url, json=data)
     elif request.method == 'GET':
         data = json.dumps(request.args)
-        response = requests.get(url, data=data)
+        response = requests.get(url, json=data)
     return Response(response, mimetype='application/json', status=response.status_code)
 
 app.register_blueprint(blueprint)
